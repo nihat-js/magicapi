72dcac71c5516e2f3da370d1895019cc10d1c1b8
nihat.js <abdullazadenihat@gmail.com>
2023-01-18 13:22:19 +0400

    users added

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4140
 
+const users = [{
+  id:0,
+  username : 'nihat',
+  password : 'aasfsa',
+  register : {
+    time : "15/01/2023"
+  }
+
+}]
+
+
 app.get('/', (req, res) => {
    res.send('This is index')
 
@@ -13,15 +24,7 @@ app.get('/users',(req,res)=>{
 
 
 
-const users = [{
-  id:0,
-  username : 'nihat',
-  password : 'aasfsa',
-  register : {
-    time : "15/01/2023"
-  }
 
-}]
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
 })
